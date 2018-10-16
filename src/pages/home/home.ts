d526bfbf3f621c03d46bdc8385138dc7f198c4a4
alumno Secundaria <al3201@cedi.local>
2018-10-16 08:05:29 -0500

    QUIZ1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LamparaPage } from '../lampara/lampara';
+import { SmartphonePage } from '../smartphone/smartphone';
+import { TelevisionPage } from '../television/television';
+import { BocinaPage } from '../bocina/bocina';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+
 export class HomePage {
+
+  Lampara= LamparaPage;
+  Smartphone = SmartphonePage;
+  tv=TelevisionPage;
+  bocina=BocinaPage;
+
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  
+    clickLAM(){
+
+    this.navCtrl.push(this.Lampara);
+
+    }
+
+    clickSMART(){
+
+      this.navCtrl.push(this.Smartphone);
+  
+      }
+
+      clicktv(){
+
+        this.navCtrl.push(this.tv);
+    
+        }
+
+        clickBOC(){
+
+          this.navCtrl.push(this.bocina);
+      
+          }
+
+
+
+    
+ 
+
 
 }
